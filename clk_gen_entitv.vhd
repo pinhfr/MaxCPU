6ddf70a266a00cb7ea9fd221c6bc09834eba79ea
Bruno <bruno.quercia@telecom-bretagne.eu>
2016-06-03 11:06:05 +0200

    Daten von den Folien addiert

---
+++ clk_gen_entitv.vhd
@@ -0,0 +1,26 @@
+entity ClkGen is
+
+	generic(init_value : bit := '1';
+		init_delay : time := 1 ns;
+		T_high, T_low : time := 1 ns;
+		T_active : time := 100 ns);
+
+	port(clk : buffer bit:=init_value);
+
+end ClkGen;
+
+architecture dataflow of ClkGen is
+
+begin
+	clk <= not init_value, init_value after init_delay
+
+	when now = 0 ns else
+		'1' after T_low
+
+	when clk = '0' and now > 0 ns and now < T_active else
+		'0' after T_high
+
+	when clk = '1' and now > 0 ns and now < T_active else
+		clk;
+
+end dataflow;
---
+++ memory_rw_pack_timed.vhd
@@ -0,0 +1,54 @@
+package memory_rw_pack_timed is
+
+	subtype word is integer range 0 to 4095;
+
+	procedure memory_read (constant Td : in time;
+			       constant r_addr : in word;
+			       variable r_data : out word;
+			       signal w_en : out boolean;
+			       signal addr : out word;
+			       signal data_from_memory: in word);
+
+	procedure memory_write (constant Td : in time;
+			        constant w_addr : in word;
+			        constant w_data : in word;
+			        signal w_en : out boolean;
+			        signal addr : out word;
+			        signal data_to_memory: out word);
+
+end memory_rw_pack_timed;
+
+package body memory_rw_pack_timed is
+
+	procedure memory_write (constant Td : in time;
+			        constant w_addr : in word;
+			        constant w_data : in word;
+			        signal w_en : out boolean;
+			        signal addr : out word;
+			        signal data_to_memory: out word) is
+	begin
+		assert Td > 0 ns;
+		addr <= w_addr;
+		data_to_memory <= w_data;
+		w_en <= TRUE;
+		wait for Td;
+		w_en <= FALSE;
+		wait for Td;
+	end memory_write;
+	
+	procedure memory_read (constant Td : in time;
+			       constant r_addr : in integer range 0 to 4095;
+			       variable r_data : out word;
+			       signal w_en : out boolean;
+			       signal addr : out word;
+			       signal data_from_memory: in word) is
+	begin
+		assert Td > 0 ns;
+		addr <= r_addr;
+		w_en <= FALSE;
+		wait for Td;
+		r_data := data_from_memory;
+		wait for Td;
+	end memory_read;
+
+end memory_rw_pack_timed;
